7464ddeef16f441fec17dd082529061c5a9c0da2
Harol <harol@HaroltekiMacBook-Pro.local>
2022-04-17 14:49:29 -0500

    add tests cases

--- src/tests/api/v1/test_product.py
+++ src/tests/api/v1/test_product.py
@@ -1,2 +1,42 @@
-def test_api():
-    assert True
+def test_product_list_api():
+    pass
+
+
+def test_product_detail_api():
+    pass
+
+
+def test_product_view_counter():
+    pass
+
+
+def test_product_create_api():
+    pass
+
+
+def test_product_create_api_permissions():
+    pass
+
+
+def test_create_product_api_notification():
+    pass
+
+
+def test_product_update_api():
+    pass
+
+
+def test_product_update_api_permissions():
+    pass
+
+
+def test_product_update_api_notification():
+    pass
+
+
+def test_product_delete_api():
+    pass
+
+
+def test_product_delete_api_permissions():
+    pass
